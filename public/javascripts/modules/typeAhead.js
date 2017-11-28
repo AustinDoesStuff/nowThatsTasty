0b6126d0b1228e4743c453f68b53ca6f1b978863
Austin Tanner <austin.tanner04@gmail.com>
2017-11-28 15:54:47 -0600

    added search feature with indexes

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -1,4 +1,15 @@
-const axios = require('axios');
+import axios from 'axios';
+import dompurify from 'dompurify';
+
+function searchResultsHTML(stores) {
+  return stores.map(store => {
+    return `
+    <a href="/store/${store.slug}" class="search__result">
+    <strong>${store.name}</strong>
+    </a>
+    `;
+  }).join('');
+};
 
 function typeAhead(search) {
   if(!search) return;
@@ -13,17 +24,47 @@ function typeAhead(search) {
     }
 
     searchResults.style.display = 'block';
-    console.log(this.value);
+    
+    axios
+      .get(`api/search?q=${this.value}`)
+      .then(res => {
+        if(res.data.length) {
+          searchResults.innerHTML = dompurify.sanitize(searchResultsHTML(res.data));
+          return;
+        }
 
-    axios.get(`/api/search?=q${this.value}`).then((res) => {
-      console.log(res.data);
-    }).catch((err) => console.log(err));
-    // axios
-    //   .get(`/api/search?=q${this.value}`)
-    //   .then((res) => {
-    //     console.log(res.data);
-    //   })
+        searchResults.innerHTML = dompurify.sanitize(`<div class="search__result">No results for ${this.value} found 🙁</div>`);
+
+      }).catch(err => console.log(err));
   });
+  //handle keyboard inputs for up, down, enter
+  searchInput.on('keyup', (e) => {
+    if(![38,40,13]) {
+      return;
+    }
+    const activeClass = 'search__result--active';
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll('.search__result');
+
+    let next;
+
+    if(e.keyCode === 40 && current) {
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) {
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1]
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+    }
+
+    if(current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass);
+  })
 };
 
 export default typeAhead;
